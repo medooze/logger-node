80742aa7b19467794b551c541999e45c6ad49da3
Brendon Costa <bcost@dolby.com>
2024-08-30 14:39:07 +1000

    Minor test additions

--- tests/logger.js
+++ tests/logger.js
@@ -1,41 +1,76 @@
 const tap = require("tap");
 const Logger = require("../index");
 
-class NullWritableStream
+class MockWritableStream
 {
-	constructor() { }
+	constructor() 
+	{
+		this.messages = "";
+	}
 
-	write() {}
+	write(msg) 
+	{
+		this.messages += msg;
+	}
 	cork() {}
 	uncork() {}
 }
 
+function testContains(test, stream, pattern)
+{
+	if (!stream.messages.includes(pattern))
+	{
+		test.equal(stream.messages, pattern);
+	}
+	else
+	{
+		test.ok(true);
+	}
+	stream.messages = "";
+}
+
 tap.test("logging", async suite => {
+
+	// Validates the setWritableStream API and means we can check results in tests
+	const stream = new MockWritableStream();
+	Logger.setWritableStream(stream);
 
 	suite.test("basic", async test => {
 		const logger = new Logger("Test");
 		logger.error("error message");
+		testContains(test, stream, "[ERROR] Test error message");
+
 		logger.warn("warn message");
+		testContains(test, stream, "[WARN ] Test warn message");
+
 		logger.info("info message");
+		testContains(test, stream, "[INFO ] Test info message");
+
 		logger.debug("debug message");
+		testContains(test, stream, "[DEBUG] Test debug message");
+
 		test.equal(logger.getName(), "Test");
 
 		const child = logger.child("child");
 		child.error("error message");
-		child.warn("warn message");
-		child.info("info message");
-		child.debug("debug message");
-		test.equal(child.getName(), "Test:child");
+		testContains(test, stream, "[ERROR] Test:child error message");
+	});
+
+	suite.test("custom-single-line-exception-stack-trace", async test => {
+		
+		const logger = new Logger("Test");
+		try
+		{
+			throw new Error("hello world");
+		}
+		catch (e)
+		{
+			logger.error(e);
+		}
 	});
 
 	suite.test("api-setFlushTimeout", async test => {
 		Logger.setFlushTimeout(1000);
-		const logger = new Logger("Test");
-		logger.debug("debug message");
-	});
-
-	suite.test("api-setWritableStream", async test => {
-		Logger.setWritableStream(new NullWritableStream());
 		const logger = new Logger("Test");
 		logger.debug("debug message");
 	});
